5de32672e7ba21ce15da75e01ba876ad37062c89
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-19 17:42:54 -0700

    fix: Update attritute from tea_type to type in Tea factory

--- spec/factories/tea.rb
+++ spec/factories/tea.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :tea do
     name { Faker::Tea.variety }
-    tea_type { rand(1..10) } 
+    type { rand(1..10) } 
     description { Faker::Lorem.sentence }
     temperature { Faker::Number.between(from: 160, to: 212) } 
     brew_time { Faker::Number.between(from: 2, to: 7) } 
