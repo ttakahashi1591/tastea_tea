b4f3a3d401077329a0f9698c7a37be57ef495be8
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-19 20:52:55 -0700

    feat: Add Type enum to the Tea class with 8 different types

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,6 @@
 class Tea < ApplicationRecord
   has_many :subscription_teas 
   has_many :subscriptions, through: :subscription_teas
+
+  enum type: %w(black chai green herbal matcha mixed oolong white)
 end
