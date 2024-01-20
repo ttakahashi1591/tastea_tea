043b3c71cf6dd6a040f702280ad3f74d73a54a03
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-19 21:57:05 -0700

    test: Setup shell for CustomerSubscriptions Create and Destroy spec test and updated incorrect data im the test_helper file

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -2,5 +2,6 @@ class Tea < ApplicationRecord
   has_many :subscription_teas 
   has_many :subscriptions, through: :subscription_teas
 
+  self.inheritance_column = :_type_disabled # Disable STI
   enum type: %w(black chai green herbal matcha mixed oolong white)
 end
--- spec/requests/api/v0/customer_subscriptions_request_spec.rb
+++ spec/requests/api/v0/customer_subscriptions_request_spec.rb
@@ -1,11 +1,66 @@
 require 'rails_helper'
 
-describe "CustomerSubscriptions API" do
-  it "CustomerSubscriptions Index" do
-    create_list(:customer_subscription, 10)
+describe "CustomerSubscriptions API Endpoints" do
+  before(:each) do
+    load_test_data
+  end
+  
+  describe "CustomerSubcriptions Create Endpoint" do
+    describe "Happy Path" do
+      it "supports with creating the association between a customer and their new subscription" do
+        expect(@ash.subscriptions).to eq([@leafy])
+        expect(@brock.subscriptions).to eq([])
 
-    get '/api/v1/customer_subscriptions'
+        post "/api/v0/customers/#{@ash.id}/subscriptions", params: {subscription_id: @sparky.id}
 
-    expect(response).to be_successful
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+
+        expect(JSON.parse(response.body)["message"]).to eq("Successfully subscribed!")
+
+        post "/api/v1/customers/#{@brock.id}/subscriptions", params: {subscription_id: @sparky.id}
+
+        expect(JSON.parse(response.body)["message"]).to eq("Successfully subscribed!")
+
+        @ash = Customer.find(@ash.id)
+        @brock = Customer.find(@brock.id)
+
+        expect(@ash.subscriptions).to eq([@sparky])
+        expect(@brock.subscriptions).to eq([@sparky])
+      end 
+    end
+
+    describe "Sad Paths" do
+      xit "does not allow a customer to subscribe to the subscription if they are already subscribed" do
+
+        expect(@ash.subscriptions).to eq([@leafy, @splashy])
+
+        post "/api/v1/customers/#{@ash.id}/subscriptions", params: {subscription_id: @splashy.id}
+
+        expect(response).to_not be_successful
+      end
+    end
+  end
+
+  describe "CustomerSubscriptions Destroy Endpoint" do
+    describe "Happy Path" do
+      it "supports with destroying (unsubscribe) an association between a cusotmer and their subscription" do
+        expect(@ash.subscriptions).to eq([@leafy, @spalshy])
+
+        delete "/api/v1/customers/#{@ash.id}/subscriptions", params: {subscription_id: @spalshy.id}
+
+        expect(response).to be_successful
+        expect(response.status).to eq(204)
+        expect(response.body).to eq("")
+
+        @ash = Customer.find(@ash.id)
+
+        expect(@ash.subscriptions).to eq([@leafy])
+      end
+    end
+
+    describe "Sad Paths" do
+      
+    end
   end
 end
