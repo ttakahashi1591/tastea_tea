daf9d602148952916dd36b3c0d0565057216972f
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-19 20:28:39 -0700

    feat: Add CustomerSubscriptions and SubcriptionTea associations tests

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -1,4 +1,4 @@
 class CustomerSubscription < ApplicationRecord
-  belongs_to :customer_id
-  belongs_to :subscription_id
+  belongs_to :customer
+  belongs_to :subscription
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   describe "associations" do
     it { should have_many :subscription_teas }
     it { should have_many(:teas).through(:subscription_teas) }
