e0053d0a702abd2788dded89d576ce2ea549fbdd
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-21 17:43:50 -0700

    refactor: Update status migration to have a deafult of 0 "active"

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_21_220244) do
     t.bigint "subscription_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["customer_id"], name: "index_customer_subscriptions_on_customer_id"
     t.index ["subscription_id"], name: "index_customer_subscriptions_on_subscription_id"
   end
