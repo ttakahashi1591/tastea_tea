bf2c2ebad2f9e09d6e095b38e25192e947e8b19b
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-21 17:10:17 -0700

    feat: Setup CustomerSubscriptions index action in routes and controller and replace destroy with update since we want to keep all order types as a record

--- app/controllers/api/v0/customer_subscriptions_controller.rb
+++ app/controllers/api/v0/customer_subscriptions_controller.rb
@@ -1,15 +1,21 @@
 class Api::V0::CustomerSubscriptionsController < ApplicationController 
+  def index 
+    customer = Customer.find(params[:customer_id])
+    
+    render json: CustomerSubscriptionsSerializer.new(customer).to_json
+  end
+  
   def create 
     customer = CustomerSubscription.create!(customer_subscription_params)
 
     render json: {message: "Successfully subscribed!"}, status: 200
   end
 
-  def destroy 
-    subscription = CustomerSubscription.find_by(customer_subscription_params)
+  # def destroy 
+  #   subscription = CustomerSubscription.find_by(customer_subscription_params)
 
-    subscription.destroy
-  end
+  #   subscription.destroy
+  # end
 
   private 
 
